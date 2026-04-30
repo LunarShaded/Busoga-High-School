ee53ea0a4b5ad47c98e436aceb456ff173921847
Maksym Shevchyk <maksim.shevchik@gmail.com>
2026-04-30 14:55:31 +0200

    Installing 'Box-icons' framework. Making header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Book } from '@boxicons/react';
 import './App.css'
 
 function App() {
@@ -6,7 +6,28 @@ function App() {
 
   return (
     <>
-      
+      <section id='header'>
+        <nav>
+          <div className='logo'>
+            <div>
+              <Book/>
+            </div>
+            <h1>Busoga High School</h1>
+            <p>Kamuli, Uganda</p>
+          </div>
+
+          <div className='nav-links'>
+            <ul>
+              <a href='#'><li>Home</li></a>
+              <a href='#'><li>About</li></a>
+              <a href='#'><li>Academics</li></a>
+              <a href='#'><li>Admissions</li></a>
+              <a href='#'><li>News & Events</li></a>
+              <a href='#'><li>Contact</li></a>
+            </ul>
+          </div>
+        </nav>
+      </section>
     </>
   )
 }
